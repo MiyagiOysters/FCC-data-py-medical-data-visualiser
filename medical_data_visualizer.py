b178d21952ef2102b47bb7a0631bf3585ca83025
Alvin Zhou <alvin.zw.zhou@gmail.com>
2024-09-23 12:34:51 +0000

    Update

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,35 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
+
 
 # 2
-df['overweight'] = None
+height_meters = df['height'] / 100
+df['overweight'] = df['weight'] / (height_meters) ** 2
+overweight_mask = df['overweight'] > 25
+df['overweight'] = np.where(overweight_mask, 1, 0)
+
 
 # 3
+cholesterol_mask = df['cholesterol'] == 1
+glucose_mask = df['gluc'] == 1
+df['cholesterol'] = np.where(cholesterol_mask, 0, 1)
+df['gluc'] = np.where(glucose_mask, 0, 1)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    
 
     # 6
-    df_cat = None
     
 
     # 7
 
 
-
     # 8
-    fig = None
 
 
     # 9
