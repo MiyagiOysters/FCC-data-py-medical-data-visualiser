9f0f75b65e9a37343b1f9b568461721c71727e2e
Alvin Zhou <alvin.zw.zhou@gmail.com>
2024-09-25 04:02:08 +0000

    solution works!

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -24,13 +24,15 @@ df['gluc'] = np.where(glucose_mask, 0, 1)
 # 4
 def draw_cat_plot():
     # 5
+    df_cat = df.melt(id_vars='cardio', value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     
 
     # 6
+    df_cat = df_cat.groupby(by=['cardio', 'variable', 'value']).value_counts().reset_index(name='total')
+
     
-
     # 7
-
+    fig = sns.catplot(data=df_cat, x='variable', y='total', col='cardio', kind='bar', hue='value').fig
 
     # 8
 
@@ -43,22 +45,22 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    bp_mask = df['ap_lo'] <= df['ap_hi']
+    height_mask = (df['height'] >= df['height'].quantile(0.025)) & (df['height'] <= df['height'].quantile(0.975))
+    weight_mask = (df['weight'] >= df['weight'].quantile(0.025)) & (df['weight'] <= df['weight'].quantile(0.975))
+    df_heat = df[height_mask & weight_mask & bp_mask] 
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
-
+    fig, ax = plt.subplots(figsize=(11, 9))
+    
     # 15
-
-
+    sns.heatmap(corr, annot=True, mask=mask, fmt='.1f')
 
     # 16
     fig.savefig('heatmap.png')
